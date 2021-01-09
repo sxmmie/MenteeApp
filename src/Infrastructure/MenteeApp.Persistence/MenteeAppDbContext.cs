db3b473318222a1cb7bc33dc1aa1aa684fe8dd6b
Sxmmie <samuelumohjnr@gmail..com>
2021-01-09 08:16:17 +0100

    Created the MenteeApContext and included the entities
    Added IGenericRepository and the SQLServer configuration in the StartUp class.

---
+++ src/Infrastructure/MenteeApp.Persistence/MenteeAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using MenteeApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenteeApp.Persistence
+{
+    public class MenteeAppDbContext : DbContext
+    {
+        public MenteeAppDbContext(DbContextOptions<MenteeAppDbContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<MeetUp> MeetUps { get; set; }
+        public DbSet<Mentee> Mentees { get; set; }
+        public DbSet<Mentor> Mentors { get; set; }
+    }
+}
